b2102f8f2917dbbbf04d3dec65cd8f8434b5957d
Juan Diego Trujillo <juandiegotm1999@gmail.com>
2023-12-07 13:07:32 +0100

    chore(script): script handles with the responsae data and errors. Added a request to reservate session and other refactors stuff.

--- scraper.py
+++ scraper.py
@@ -5,34 +5,82 @@ import base64
 import RequestManager
 import AudioManager
 import NotificationManager
-
 from helpers import generate_dates_interval
 
 START_DATE = '2024-01-22'
 INTERVAL_IN_S = 60
 
-def get_exclude_days(start_date, end_date):
-    answer = RequestManager.request_exclude_days(start_date, end_date)
-    if not answer:
-        print("No se puedo recuperar los días excluidos, renovando token...")
-        renovado = False
-        i = 0
-        while i < 10:
-            print(f"Intento {i}")
-            renovado = renovate_session()
-            
-            if renovado:
-                print("Sesión renovada")
-                answer = RequestManager.request_exclude_days(start_date, end_date)
-                break
-            else:
-                print("Intento fallido, volviendo a internar...")
-                time.sleep(5)
-            i+=1
+def solve_captcha(captcha_audio):
+    audio_wav = AudioManager.decode_to_wav(captcha_audio)
+    binary_audio_data = audio_wav.read()
+    return AudioManager.convertir_audio_a_texto(io.BytesIO(binary_audio_data))
+
+def renovate_session():
+    # First, we have to make de reservation step. 
+    reservation_step()
+    renovado = False
+    i = 0
+    while i < 10:
+        print(f"Intento {i}")
+        renovado = captcha_step()
         
-        if not renovado:
-            raise Exception("No fue posible renovar el token luego de 10 intentos")
-    return answer
+        if renovado:
+            print("Catpcha solucionado con exito")
+            break
+        else:
+            print("Intento fallido, volviendo a internar...")
+            time.sleep(5)
+        i+=1
+    
+    if not renovado:
+        raise Exception("No fue posible renovar el token luego de 10 intentos")
+
+    return renovado
+
+def reservation_step():
+    response_reservate_sesion = RequestManager.reservate_sesion()
+    body = response_reservate_sesion.json()
+
+    print(body)
+
+    if not body['session']:
+        Exception("El token no existe o es demasiado viejo. Renuevelo y vuelva a internarlo")
+
+    return True if response_reservate_sesion.status_code == 200 else False
+    
+
+def captcha_step():
+    get_captcha_response = RequestManager.get_captcha()
+    catpcha, csrf_token = get_captcha_response.json(), get_captcha_response.headers['x-gouv-csrf']
+
+    # Decodificar la cadena binaria
+    binary_audio_data = base64.b64decode(catpcha['audio'])
+
+    solution = solve_captcha(binary_audio_data)
+    if len(solution) == 4:
+        response_send_captcha = RequestManager.send_captch(solution, csrf_token)
+        return True if response_send_captcha.status_code == 200 else False
+    return False
+
+def handle_exclude_days(response):
+    if response.status_code == 200 and len(response.text) == 0:
+        raise Exception('El header x-gouv-app-id está vencido, por favor renuevelo')
+    elif response.status_code == 429:
+        raise Exception("El servidor detectó muchas peticiones en poco tiempo. Pausando 5 minutos")
+
+def get_exclude_days(start_date, end_date):
+    response = RequestManager.request_exclude_days(start_date, end_date)
+    handle_exclude_days(response)
+
+    if response.status_code != 404:
+        return response.json()
+    
+    print("No se puedo recuperar los días excluidos, renovando token...")
+    
+    new_response = RequestManager.request_exclude_days(start_date, end_date)
+    if new_response.status_code != 200:
+        raise Exception("Hubo un problema inesperado: ", new_response.status_code, new_response.text)
+    return new_response.json()
 
 
 def avaliable_dates(start_date, end_date):
@@ -42,23 +90,7 @@ def avaliable_dates(start_date, end_date):
     
     possible_days = generate_dates_interval(end_date, end_date)
     return list(possible_days-set(exclude_days))
-        
 
-def solve_captcha(captcha_audio):
-    audio_wav = AudioManager.decode_to_wav(captcha_audio)
-    binary_audio_data = audio_wav.read()
-    return AudioManager.convertir_audio_a_texto(io.BytesIO(binary_audio_data))
-
-def renovate_session():
-    data, csrf_token = RequestManager.get_captcha()
-    audio_mp3_encoded = data['audio']
-
-    # Decodificar la cadena binaria
-    binary_audio_data = base64.b64decode(audio_mp3_encoded)
-
-    captcha = solve_captcha(binary_audio_data)
-    print("La resolución del captcha es: " + captcha)
-    return RequestManager.send_captch(captcha, csrf_token)
 
 def main():
     print("Iniciando servicio.")
@@ -72,10 +104,7 @@ def main():
 
     while True:
         dates = avaliable_dates(START_DATE, end_date)
-        if dates == None:
-            print("Token vencido, renuevalo!")
-            break
-        elif len(dates) == 0:
+        if len(dates) == 0:
             print(f"No hay fechas disponibles. Volviendo a intentar en {INTERVAL_IN_S} segundos...")
             time.sleep(INTERVAL_IN_S)
         else:
