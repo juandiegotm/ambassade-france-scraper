0c60639eb2f0577d8d6effe9ed030c5ca859f949
Juan Diego Trujillo <juandiegotm1999@gmail.com>
2023-12-08 07:24:08 +0100

    feat(session_id): finally added the complete and correct flow of authentication system.

--- RequestManager.py
+++ RequestManager.py
@@ -1,73 +1,140 @@
 import json
 import requests
+import base64
 from helpers import transform_date_format
+import time
+import io
 
-SESSION_ID = '65712657236bf757bfe045b7'
-GOUV_APP_ID = 'fr.gouv$+rj_BOHWPWrqOyqXMys5lAuEbWPwnuoq8%%a4ee555b-c7dd-4979-a380-4ddeba9e1dfa-meae-ttc'
+import AudioManager
 
 BASE_PATH = 'https://api.consulat.gouv.fr/api/team/6230a5f8eb8eddc6026c2f86/reservations/exclude-days'
 GET_INTERVAL_PATH = 'https://api.consulat.gouv.fr/api/team/6230a5f8eb8eddc6026c2f86/reservations/get-interval?serviceId=6233529437d20079e6271bd9'
 
-def send_captch(captcha, csrf_token):
-    url = "https://api.consulat.gouv.fr/api/team/6230a5f8eb8eddc6026c2f86/reservations-session"
+class RequestManager:
+    def __init__(self) -> None:
+        self.gouv_app_id = self.__get_gouv_app_id()
+        self.session_id = self.__renovate_session()
 
-    payload = json.dumps({
-        "sessionId": SESSION_ID,
-        "captcha": "troov_c_" + captcha
-    })
-    headers = {
-        'x-gouv-app-id': GOUV_APP_ID,
-        'Content-Type': 'application/json',
-        'x-gouv-ck': csrf_token,
-        'x-csrf-token': csrf_token
-    }
+    def update_session(self):
+        self.session_id = self.__renovate_session()
 
-    return requests.request("POST", url, headers=headers, data=payload)
-
-def get_captcha():
-    url = "https://api.consulat.gouv.fr/api/captcha?locale=es"
-    payload = {}
-    headers = {
-        'x-gouv-app-id': GOUV_APP_ID
-    }
-
-    return requests.request("GET", url, headers=headers, data=payload)
-
-def get_interval():
-    headers = {
-        'x-gouv-app-id': GOUV_APP_ID,
-    }
-    response = requests.request("GET", GET_INTERVAL_PATH, headers=headers)
-    return response.json()
-
-def request_exclude_days(start_date, end_date):
-    request_body = {
-        "start": transform_date_format(start_date),
-        "end": transform_date_format(end_date),
-        "session": {
-            "623a31e505be16413d5f71ce": 1
-        },
-        "sessionId": SESSION_ID
-    }
-
-    headers = {
-        'Content-Type': 'application/json',
-        'x-gouv-app-id': GOUV_APP_ID
-    }
-
-    return requests.post(BASE_PATH, data=json.dumps(request_body), headers=headers)
-
-def reservate_sesion():
-    url = "https://api.consulat.gouv.fr/api/team/6230a5f8eb8eddc6026c2f86/reservations-session"
+    def __request_hand_shake(self):
+        url = "https://api.consulat.gouv.fr/api/handshake"
+        return requests.request("HEAD", url)
     
-    payload = {
-        'sessionId': SESSION_ID
-    }
-    
-    headers = {
-        'x-gouv-app-id': GOUV_APP_ID
-    }
+    def __get_gouv_app_id(self):
+        return self.__request_hand_shake().headers['x-gouv-app-id']
 
-    return requests.request("GET", url, headers=headers, data=payload)
+    def __send_captch(self, captcha, csrf_token):
+        url = "https://api.consulat.gouv.fr/api/team/6230a5f8eb8eddc6026c2f86/reservations-session"
+
+        payload = json.dumps({
+            "sessionId": None,
+            "captcha": "troov_c_" + captcha
+        })
+        headers = {
+            'x-gouv-app-id': self.gouv_app_id ,
+            'Content-Type': 'application/json',
+            'x-gouv-ck': csrf_token,
+            'x-csrf-token': csrf_token
+        }
+
+        return requests.request("POST", url, headers=headers, data=payload)
+
+    def get_captcha(self):
+        url = "https://api.consulat.gouv.fr/api/captcha?locale=es"
+        payload = {}
+        headers = {
+            'x-gouv-app-id': self.gouv_app_id 
+        }
+
+        return requests.request("GET", url, headers=headers, data=payload)
+
+    def get_interval(self):
+        headers = {
+            'x-gouv-app-id': self.gouv_app_id
+        }
+        response = requests.request("GET", GET_INTERVAL_PATH, headers=headers)
+        return response.json()
+
+    def request_exclude_days(self, start_date, end_date):
+        request_body = {
+            "start": transform_date_format(start_date),
+            "end": transform_date_format(end_date),
+            "session": {
+                "623a31e505be16413d5f71ce": 1
+            },
+            "sessionId": self.session_id
+        }
+
+        headers = {
+            'Content-Type': 'application/json',
+            'x-gouv-app-id': self.gouv_app_id 
+        }
+
+        return requests.post(BASE_PATH, data=json.dumps(request_body), headers=headers)
+
+    def reservate_sesion(self):
+        url = "https://api.consulat.gouv.fr/api/team/6230a5f8eb8eddc6026c2f86/reservations-session"
+        
+        payload = {
+            'sessionId': self.session_id
+        }
+        
+        headers = {
+            'x-gouv-app-id': self.gouv_app_id 
+        }
+
+        return requests.request("GET", url, headers=headers, data=payload)
+    
+    def __renovate_session(self):
+        session_id = None
+        i = 0
+        while i < 10:
+            print(f"Intento {i}")
+            session_id = self._get_session_id()
+            
+            if session_id:
+                print("Catpcha solucionado con exito")
+                break
+            else:
+                print("Intento fallido, volviendo a internar...")
+                time.sleep(5)
+            i+=1
+        
+        if not session_id:
+            raise Exception("No fue posible renovar el token luego de 10 intentos")
+
+        return session_id
 
     
+    def _get_session_id(self):
+        get_captcha_response = self.get_captcha()
+        catpcha, csrf_token = get_captcha_response.json(), get_captcha_response.headers['x-gouv-csrf']
+
+        binary_audio_data = base64.b64decode(catpcha['audio'])
+
+        solution = self.solve_captcha(binary_audio_data)
+        if len(solution) != 4:
+            return None
+        
+        response_send_captcha = self.__send_captch(solution, csrf_token)
+        try:
+            body = response_send_captcha.json()
+            return body['_id']
+        except:
+            return None
+        
+    def solve_captcha(self, captcha_audio):
+        audio_wav = AudioManager.decode_to_wav(captcha_audio)
+        binary_audio_data = audio_wav.read()
+        return AudioManager.convertir_audio_a_texto(io.BytesIO(binary_audio_data))
+    
+    def reservation_step(self):
+        response_reservate_sesion = self.reservate_sesion()
+        body = response_reservate_sesion.json() 
+
+        if not body['session']:
+            Exception("El token no existe o es demasiado viejo. Renuevelo y vuelva a internarlo")
+
+        return True if response_reservate_sesion.status_code == 200 else False
--- scraper.py
+++ scraper.py
@@ -1,83 +1,33 @@
 import time
-import io
-import base64
 
-import RequestManager
-import AudioManager
-import NotificationManager
+from RequestManager import RequestManager
+from NotificationManager import NotificationManager
 from helpers import generate_dates_interval
 
 START_DATE = '2024-01-22'
-INTERVAL_IN_S = 60
+BOT_TOKEN = ''
+CHAT_ID = ''
+INTERVAL_IN_S = 120
+request_manager =  RequestManager()
 
-def solve_captcha(captcha_audio):
-    audio_wav = AudioManager.decode_to_wav(captcha_audio)
-    binary_audio_data = audio_wav.read()
-    return AudioManager.convertir_audio_a_texto(io.BytesIO(binary_audio_data))
-
-def renovate_session():
-    # First, we have to make de reservation step. 
-    reservation_step()
-    renovado = False
-    i = 0
-    while i < 10:
-        print(f"Intento {i}")
-        renovado = captcha_step()
-        
-        if renovado:
-            print("Catpcha solucionado con exito")
-            break
-        else:
-            print("Intento fallido, volviendo a internar...")
-            time.sleep(5)
-        i+=1
-    
-    if not renovado:
-        raise Exception("No fue posible renovar el token luego de 10 intentos")
-
-    return renovado
-
-def reservation_step():
-    response_reservate_sesion = RequestManager.reservate_sesion()
-    body = response_reservate_sesion.json()
-
-    print(body)
-
-    if not body['session']:
-        Exception("El token no existe o es demasiado viejo. Renuevelo y vuelva a internarlo")
-
-    return True if response_reservate_sesion.status_code == 200 else False
-    
-
-def captcha_step():
-    get_captcha_response = RequestManager.get_captcha()
-    catpcha, csrf_token = get_captcha_response.json(), get_captcha_response.headers['x-gouv-csrf']
-
-    # Decodificar la cadena binaria
-    binary_audio_data = base64.b64decode(catpcha['audio'])
-
-    solution = solve_captcha(binary_audio_data)
-    if len(solution) == 4:
-        response_send_captcha = RequestManager.send_captch(solution, csrf_token)
-        return True if response_send_captcha.status_code == 200 else False
-    return False
 
 def handle_exclude_days(response):
     if response.status_code == 200 and len(response.text) == 0:
-        raise Exception('El header x-gouv-app-id está vencido, por favor renuevelo')
+        raise Exception('Hubo una excepción extraña.')
     elif response.status_code == 429:
         raise Exception("El servidor detectó muchas peticiones en poco tiempo. Pausando 5 minutos")
 
 def get_exclude_days(start_date, end_date):
-    response = RequestManager.request_exclude_days(start_date, end_date)
+    response = request_manager.request_exclude_days(start_date, end_date)
     handle_exclude_days(response)
 
     if response.status_code != 404:
         return response.json()
     
-    print("No se puedo recuperar los días excluidos, renovando token...")
-    
-    new_response = RequestManager.request_exclude_days(start_date, end_date)
+    print("No se puedo recuperar los días excluidos, renovando sesion...")
+    request_manager.update_session()
+
+    new_response = request_manager.request_exclude_days(start_date, end_date)
     if new_response.status_code != 200:
         raise Exception("Hubo un problema inesperado: ", new_response.status_code, new_response.text)
     return new_response.json()
@@ -85,19 +35,17 @@ def get_exclude_days(start_date, end_date):
 
 def avaliable_dates(start_date, end_date):
     exclude_days = get_exclude_days(start_date, end_date)
-    if not exclude_days:
-        return None
-    
-    possible_days = generate_dates_interval(end_date, end_date)
-    return list(possible_days-set(exclude_days))
+    possible_days = generate_dates_interval(start_date, end_date)
 
+    return list(possible_days-set(exclude_days))
 
 def main():
     print("Iniciando servicio.")
+    notificator = NotificationManager(BOT_TOKEN, CHAT_ID)
 
     # Even the server give us a start_date, 
     # we are interested in use the date defined by the user. 
-    interval_limits = RequestManager.get_interval()
+    interval_limits = request_manager.get_interval()
     end_date = interval_limits['end']
     
     print(f"Se buscarán citas en el periodo {START_DATE} y {end_date}")
@@ -106,11 +54,10 @@ def main():
         dates = avaliable_dates(START_DATE, end_date)
         if len(dates) == 0:
             print(f"No hay fechas disponibles. Volviendo a intentar en {INTERVAL_IN_S} segundos...")
-            time.sleep(INTERVAL_IN_S)
         else:
-            NotificationManager.send_notification()
             dates.sort()
-            print(dates)
-            break
+            notificator.notify_available_days(dates)
+            print(f"¡Fechas encontradas! Volviendo a intentar en {INTERVAL_IN_S} segundos...")
+        time.sleep(INTERVAL_IN_S)
 
 main()
